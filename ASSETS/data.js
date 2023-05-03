4011b567d1d10724cd5febabfa3a09f72eae9984
yiuchl <yiuc318@newschool.edu>
2023-05-03 15:59:04 -0400

    Beautiful!
    Draw the graffiti

--- ASSETS/data.js
+++ ASSETS/data.js
@@ -171,6 +171,46 @@ function closeNav() {
   document.getElementById("hamburger").style.marginLeft = "0";
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+  modal.style.display = "inline-block";
+}
+btn2.onclick = function () {
+  modal2.style.display = "inline-block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+span2.onclick = function () {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+window.onclick = function (event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
 // "Please, that's so fetch" retrieves Jason (gets the JSON data)
 fetch("./ASSETS/2022.json")
   .then(function (response) {
@@ -179,3 +219,32 @@ fetch("./ASSETS/2022.json")
   .then(function (collection) {
     renderItems(collection);
   })
+
+// text scamble
+// classes container and scramble
+let interval
+
+const element = document.querySelector('.scramble')
+const originalText = element.innerText
+
+const randomInt = max => Math.floor(Math.random() * max)
+const randomFromArray = array => array[randomInt(array.length)]
+
+const scrambleText = text => {
+  const chars = '*>⋆&@#✧.%$-_:/✿;?!'.split('')
+  return text
+    .split('')
+    .map(x => randomInt(3) > 1 ? randomFromArray(chars) : x)
+    .join('')
+}
+
+element.addEventListener('mouseover', () => {
+  interval = setInterval(() =>
+    element.innerText = scrambleText(originalText)
+    , 100)
+})
+
+element.addEventListener('mouseout', () => {
+  clearInterval(interval)
+  element.innerText = originalText
+})
